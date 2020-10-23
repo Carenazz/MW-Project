9f55c9f99891491299e9e4cb8afe05bf5e138fb3
Carenazz <73272083+Carenazz@users.noreply.github.com>
2020-10-23 11:02:58 +0200

    VikingHP system (First commit)

--- Assets/Code/Enemies/Boss/Viking/VikingHP.cs
+++ Assets/Code/Enemies/Boss/Viking/VikingHP.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class VikingHP : MonoBehaviour
 {
-    // Start is called before the first frame update
+    int currentHealth, maxHealth = 500, regen;
+    public float regenTimer;
+
     void Start()
     {
-        
+        currentHealth = maxHealth;
+        regenTimer = 5f;
+        regen = 5;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (currentHealth > 0)
+        {
+            regenTimer  -= Time.deltaTime;
+            if (regenTimer <= 0)
+            {
+                Regenerate();
+                regenTimer = 5f;
+            }
+        }
+        else if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+    }
+
+    public void Regenerate()
+    {
+        currentHealth += regen;
+    }
+
+    void Die()
+    {
+        this.enabled = false;
     }
 }
