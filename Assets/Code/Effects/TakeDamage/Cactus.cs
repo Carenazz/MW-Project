ecc33ffea1897f2f78943a156ce868511b0d2871
Carenazz <73272083+Carenazz@users.noreply.github.com>
2020-10-23 11:47:10 +0200

    Cactus damage
    A soft-lock due to new jumping system on desert map, going to make cactus's prick you to death there.

---
+++ Assets/Code/Effects/TakeDamage/Cactus.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cactus : MonoBehaviour
+{
+    public int damage;
+
+    void Start()
+    {
+        damage = 5;
+    }
+
+    void Update()
+    {
+        
+    }
+}
