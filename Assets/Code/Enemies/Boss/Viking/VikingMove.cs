177cf2a4a5e8705943f9f993de238ca1c7f962e3
Carenazz <73272083+Carenazz@users.noreply.github.com>
2020-10-26 13:03:26 +0100

    Viking Animation Fix + Visual

--- Assets/Code/Enemies/Boss/Viking/VikingMove.cs
+++ Assets/Code/Enemies/Boss/Viking/VikingMove.cs
@@ -25,13 +25,13 @@ public class VikingMove : MonoBehaviour
         {
             transform.localScale = flipped;
             transform.Rotate(0f, 180f, 0f);
-            isFlipped = false;
+            isFlipped = true;
         }
         else if (transform.position.x > player.position.x && isFlipped)
         {
             transform.localScale = flipped;
             transform.Rotate(0f, 180f, 0f);
-            isFlipped = true;
+            isFlipped = false;
         }
     }
 
