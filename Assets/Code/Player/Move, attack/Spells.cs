803eb175b4224044c4d8575dabe6dd805e501764
Carenazz <73272083+Carenazz@users.noreply.github.com>
2020-10-28 08:14:45 +0100

    Code - Fireball
    Prefab method of fireball cast.
    Now working on animations for it.

---
+++ Assets/Code/Player/Move, attack/Spells.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spells : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject fireSpell;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(fireSpell, firePoint.position, firePoint.rotation);
+    }
+}
---
+++ Assets/Fire.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    public float speed = 9f;
+    public int damage = 20;
+    public Rigidbody2D rigid;
+
+    void Start()
+    {
+        rigid.velocity = transform.right * speed;    
+    }
+
+    private void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        EnemyHP enemy = hitInfo.GetComponent<EnemyHP>();
+        if (enemy != null)
+        {
+            enemy.TakeDamage(damage);
+        }
+        Destroy(gameObject);
+    }
+}
