17d0e19e9b1d530c68c5272300471338f2958857
Carenazz <73272083+Carenazz@users.noreply.github.com>
2020-11-02 13:15:56 +0100

    UI - Inventory.

---
+++ Assets/Code/Menu/InvMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvMenu : MonoBehaviour
+{
+    public static bool isInvOpen = false;
+
+    public GameObject UI_Inventory;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            if (!isInvOpen)
+            {
+                Open();
+            }
+            else
+            {
+                Close();
+            }
+        }
+    }
+
+    void Open()
+    {
+        UI_Inventory.SetActive(true);
+        isInvOpen = true;
+    }
+
+    void Close()
+    {
+        UI_Inventory.SetActive(false);
+        isInvOpen = false;
+    }
+}
