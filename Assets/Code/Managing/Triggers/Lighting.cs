c32625b4abfe17c9940cee8c81871e66fb6cc284
Carenazz <73272083+Carenazz@users.noreply.github.com>
2020-11-05 11:16:46 +0100

    Final Light Code. (Now Working)
    Can always be changed later, but for now it works as it should. Time to re-work mine map to fit dark theme.

--- Assets/Code/Managing/Triggers/Lighting.cs
+++ Assets/Code/Managing/Triggers/Lighting.cs
@@ -4,28 +4,5 @@ using UnityEngine;
 
 public class Lighting : MonoBehaviour
 {
-    public bool ena = false;
-    PlayerLight turn;
-    public GameObject lights;
 
-    private void Start()
-    {
-        turn = lights.GetComponent<PlayerLight>();
-    }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.transform.tag == "Player")
-        {
-            if (!ena)
-            {
-                turn.TurnOn();
-                ena = true;
-            }
-            else if (ena)
-            {
-                turn.TurnOff();
-                ena = false;
-            }
-        }
-    }
 }
--- Assets/Code/Player/PlayerLight.cs
+++ Assets/Code/Player/PlayerLight.cs
@@ -5,7 +5,31 @@ using UnityEngine;
 public class PlayerLight : MonoBehaviour
 {
     public GameObject playerLight;
+    private bool ena = false;
+    private float timer = 0f;
 
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "LightsTrigger")
+        {
+            if (!ena && timer <= 0f)
+            {
+                TurnOn();
+                ena = true;
+                timer = 5f;
+            }
+            else if (ena && timer <= 0f)
+            {
+                TurnOff();
+                ena = false;
+                timer = 5f;
+            }
+        }
+    }
     public void TurnOn()
     {
         playerLight.SetActive(true);
