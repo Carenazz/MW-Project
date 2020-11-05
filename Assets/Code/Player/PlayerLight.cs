046f8edb0009ca515ed8e739410fda62d69d41c9
Carenazz <73272083+Carenazz@users.noreply.github.com>
2020-11-05 08:32:06 +0100

    Making playerlight.

---
+++ Assets/Code/Player/PlayerLight.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLight : MonoBehaviour
+{
+    public PlayerControls player;
+
+
+    void Start()
+    {
+        player = FindObjectOfType<PlayerControls>();    
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+}
