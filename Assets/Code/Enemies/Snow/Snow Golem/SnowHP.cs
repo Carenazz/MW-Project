c3d968a600c24e4f809aba03c424fa36de69924e
Carenazz <73272083+Carenazz@users.noreply.github.com>
2020-11-13 11:33:42 +0100

    A bit of code cleanup.

--- Assets/Code/Enemies/Snow/Snow Golem/SnowHP.cs
+++ Assets/Code/Enemies/Snow/Snow Golem/SnowHP.cs
@@ -8,34 +8,47 @@ public class SnowHP : MonoBehaviour
     [SerializeField]
     private int maxHealth = 250, currentHealth;
 
+    #region Components
     AIPath path;
     Seeker seek;
     Animator anim;
     Rigidbody2D rb;
+    Collider2D m_coll;
+    #endregion
 
     void Start()
     {
         currentHealth = maxHealth;
+
+        #region Getting components
         anim = GetComponent<Animator>();
         path = GetComponent<AIPath>();
         seek = GetComponent<Seeker>();
         rb = GetComponent<Rigidbody2D>();
+        m_coll = GetComponent<Collider2D>();
+        #endregion
     }
 
     void Update()
     {
         if (currentHealth <= 0)
         {
-            this.enabled = false;
-            path.enabled = false;
-            seek.enabled = false;
-            anim.SetBool("Death", true);
-            rb.gravityScale = 2;   
+            Die();
         }
     }
 
     public void TakeDamage(int amount)
     {
         currentHealth -= amount;
+    }
+
+    public void Die()
+    {
+        this.enabled = false;
+        path.enabled = false;
+        seek.enabled = false;
+        anim.SetBool("Death", true);
+        rb.gravityScale = 2;
+        m_coll.enabled = !m_coll.enabled;
     }
 }
