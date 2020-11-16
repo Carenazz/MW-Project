8e46ec746728110d8066ee569b4b7385b936c433
Carenazz <73272083+Carenazz@users.noreply.github.com>
2020-11-16 10:08:25 +0100

    Timer sat op for at undgå accidental double clicks.

--- Assets/Code/Managing/Triggers/WallOpen.cs
+++ Assets/Code/Managing/Triggers/WallOpen.cs
@@ -7,7 +7,7 @@ public class WallOpen : MonoBehaviour
     #region variables
 
     [SerializeField]
-    private float speed = 2f;
+    private float speed = 2f, timer = 1.5f;
     private bool moved = false;
 
     public GameObject target;
@@ -19,23 +19,28 @@ public class WallOpen : MonoBehaviour
     {
         RaycastHit2D playerInfo = Physics2D.Raycast(transform.position, Vector2.down, 0.5f, whatIsPlayer);
 
-        if (Input.GetKeyDown(KeyCode.E) && playerInfo == GameObject.FindGameObjectWithTag("Player"))
+        if (Input.GetKeyDown(KeyCode.E) && playerInfo == GameObject.FindGameObjectWithTag("Player") && timer <= 0f)
         {
             Move();
         }
+        else
+            timer -= Time.deltaTime;
     }
 
+    // Mål: Få target til at bevæge sig mod et andet target.
     public void Move()
     {
         if (!moved)
         {
             target.SetActive(false);
             moved = true;
+            timer = 1.5f;
         }
         else if (moved)
         {
             target.SetActive(true);
             moved = false;
+            timer = 1.5f;
         }
     }
 }
