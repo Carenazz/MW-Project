ce761ceacc14466f05486f08313e5dc70425c2a0
Carenazz <73272083+Carenazz@users.noreply.github.com>
2020-11-19 14:28:01 +0100

    Global light switch on / off code.

---
+++ Assets/Code/Managing/Triggers/GlobalLight.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalLight : MonoBehaviour
+{
+    public GameObject gLight;
+    public bool on = true;
+
+    void Switcher()
+    {
+        if (on)
+        {
+            gLight.SetActive(false); 
+            on = false;
+        }
+        else if (!on)
+        {
+            gLight.SetActive(true);
+            on = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Switcher();
+        }
+    }
+}
