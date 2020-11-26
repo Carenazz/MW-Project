d1bb7dc9c8648782a033a90353f95bc6e2839480
Carenazz <73272083+Carenazz@users.noreply.github.com>
2020-11-26 11:11:36 +0100

    Stats code + UI

---
+++ Assets/Code/Player/Stats/StatUI.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatUI : MonoBehaviour
+{
+    PlayerControls player;
+    public GameObject stats;
+    private bool isOpen = false;
+
+    private void Start()
+    {
+        player = GetComponent<PlayerControls>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            ShowStats();
+        }
+    }
+
+    void ShowStats()
+    {
+        if (!isOpen)
+        {
+            stats.SetActive(true);
+            isOpen = true;
+        }
+        else
+        {
+            stats.SetActive(false);
+            isOpen = false;
+        }
+    }
+}
