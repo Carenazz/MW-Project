53bcbb3f1f9d7afe5991c861da9f15ef7a043a92
Carenazz <73272083+Carenazz@users.noreply.github.com>
2020-11-27 08:24:04 +0100

    Trying to implement a level system.

---
+++ Assets/Code/Player/Level + Skip/ExpLevel.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExpLevel : MonoBehaviour
+{
+    public int exp, level = 1, maxExp = 150;
+    Stats stats;
+
+    private void Start()
+    {
+        stats = GetComponent<Stats>();
+    }
+
+    private void Update()
+    {
+        ExpHandler();
+        LevelStats();
+    }
+
+    void ExpHandler()
+    {
+
+        if (maxExp <= exp)
+        {
+            level++;
+            maxExp = level * maxExp;
+            exp = 0;
+        }
+    }
+
+    void LevelStats()
+    {
+        stats.agility = (stats.agility * level) / 2;
+        stats.str = (stats.str * level) / 2;
+        stats.will = (stats.will / level) * 2;
+        stats.stamina = stats.stamina + level;
+    }
+}
