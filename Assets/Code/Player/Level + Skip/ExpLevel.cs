82bc6fcbfefd732ccbd7baf5f05720add0aaadd8
Carenazz <73272083+Carenazz@users.noreply.github.com>
2020-12-01 09:30:37 +0100

    Fixed stats duplication (Handled by level handler now)

--- Assets/Code/Player/Level + Skip/ExpLevel.cs
+++ Assets/Code/Player/Level + Skip/ExpLevel.cs
@@ -12,23 +12,6 @@ public class ExpLevel : MonoBehaviour
         stats = GetComponent<Stats>();
     }
 
-    private void Update()
-    {
-        ExpHandler();
-        LevelStats();
-    }
-
-    void ExpHandler()
-    {
-
-        if (maxExp <= exp)
-        {
-            level++;
-            maxExp = level * maxExp;
-            exp = 0;
-        }
-    }
-
     void LevelStats()
     {
         stats.agility = (stats.agility * level) / 2;
