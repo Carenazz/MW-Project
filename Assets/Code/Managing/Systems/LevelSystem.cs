ece3cf6f656e25a8be13d7ba04a65b081a60c071
Carenazz <73272083+Carenazz@users.noreply.github.com>
2020-12-01 08:29:13 +0100

    LevelHandler, level showing in stats.

---
+++ Assets/Code/Managing/Systems/LevelSystem.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSystem
+{
+    // Inspired by CodeMonkey
+
+    private int level, experience, experienceNextLvl;
+
+    public LevelSystem()
+    {
+        level = 0;
+        experience = 0;
+        experienceNextLvl = 100;
+    }
+
+    public void AddExperience(int amount)
+    {
+        experience += amount;
+
+        if (experience >= experienceNextLvl)
+        {
+            level++;
+            experience -= experienceNextLvl;
+        }
+    }
+
+    public int GetLevelNumber()
+    {
+        return level;
+    }
+}
---
+++ Assets/Code/Managing/Systems/LevelTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelTest : MonoBehaviour
+{
+    private void Awake()
+    {
+        LevelSystem levelSystem = new LevelSystem();
+        Debug.Log(levelSystem.GetLevelNumber());
+        levelSystem.AddExperience(50);
+        Debug.Log(levelSystem.GetLevelNumber());
+        levelSystem.AddExperience(60);
+        Debug.Log(levelSystem.GetLevelNumber());
+    }
+}
