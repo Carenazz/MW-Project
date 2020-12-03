dec008a334ad4d70897ef84c68b7962e9de01bc6
Carenazz <73272083+Carenazz@users.noreply.github.com>
2020-12-03 08:08:17 +0100

    Stats level up code

--- Assets/Code/Player/Move, attack/Stats.cs
+++ Assets/Code/Player/Move, attack/Stats.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,6 +7,7 @@ public class Stats : MonoBehaviour
 {
     public int str, stamina, will;
     public float agility;
+    private LevelSystem levelSystem;
 
     private void Start()
     {
@@ -15,11 +17,18 @@ public class Stats : MonoBehaviour
         agility = 2f;
     }
 
-    public void LevelStats()
+    public void LevelStats(object sender, EventArgs e)
     {
         agility += 0.2f;
         str += 1;
         will += 1 / 3;
         stamina += 1;
+    }
+
+    public void SetLevelSystem(LevelSystem levelSystem)
+    {
+        this.levelSystem = levelSystem;
+
+        levelSystem.onLevelChange += LevelStats;
     }
 }
