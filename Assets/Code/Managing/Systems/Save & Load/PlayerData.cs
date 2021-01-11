c1d3d820f8d8b4572b0e8b7ec4da564136fd1929
Carenazz <73272083+Carenazz@users.noreply.github.com>
2021-01-11 08:24:39 +0100

    Working the save system into the other code values.

--- Assets/Code/Managing/Systems/Save & Load/PlayerData.cs
+++ Assets/Code/Managing/Systems/Save & Load/PlayerData.cs
@@ -5,8 +5,18 @@ using UnityEngine;
 [System.Serializable]
 public class PlayerData
 {
+    #region Variables to be saved
+
+    // Player related saving. (Experience, health, levels
     public int levels, health, expA;
     public float[] position;
+
+
+    // Stats save
+    public int str, stamina, will;
+    public float agility;
+
+    #endregion
 
     public PlayerData (PlayerControls player)
     {
@@ -18,5 +28,13 @@ public class PlayerData
         position[0] = player.transform.position.x;
         position[1] = player.transform.position.y;
         position[2] = player.transform.position.z;
+    }
+
+    public PlayerData (Stats stats)
+    {
+        str = stats.str;
+        stamina = stats.stamina;
+        will = stats.will;
+        agility = stats.agility;
     }
 }
