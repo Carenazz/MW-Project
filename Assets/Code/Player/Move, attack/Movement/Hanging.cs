18bb917c3edbb5269aa2ff8626e971f4e016a04e
Carenazz <73272083+Carenazz@users.noreply.github.com>
2021-01-22 07:28:08 +0100

    Update Hanging.cs

--- Assets/Code/Player/Move, attack/Movement/Hanging.cs
+++ Assets/Code/Player/Move, attack/Movement/Hanging.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class Hanging : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region Variables
+    private int distance;
+    private bool isHanging;
+    #endregion
+
+    #region Components and layermasks
+    public LayerMask whatIsPlatform;
+    public Animator anim;
+    #endregion
+
+    public void HangCheck()
     {
+        RaycastHit2D hangInfo = Physics2D.Raycast(transform.position, Vector2.down, distance, whatIsPlatform);
         
+        if (hangInfo.collider != null)
+        {
+            isHanging = true;
+            // Animation for hanging.
+
+            if (isHanging == true)
+            {
+                if (Input.GetKeyDown(KeyCode.W))
+                {
+                    // Animation for pull up
+                    isHanging = false;
+                }
+                else if (Input.GetKeyDown(KeyCode.S))
+                {
+                    // Animation for at slippe
+                    isHanging = false;
+                }
+            }
+        }
+        else
+        {
+            isHanging = false;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
