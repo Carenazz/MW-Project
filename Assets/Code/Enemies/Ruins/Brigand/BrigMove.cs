08c7e7ef97c66ce1e8e3ca8098b41eb4f9a51dff
Carenazz <73272083+Carenazz@users.noreply.github.com>
2021-02-03 10:58:56 +0100

    Update BrigMove.cs

--- Assets/Code/Enemies/Ruins/Brigand/BrigMove.cs
+++ Assets/Code/Enemies/Ruins/Brigand/BrigMove.cs
@@ -4,15 +4,60 @@ using UnityEngine;
 
 public class BrigMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region variables
+    Rigidbody2D rb;
+
+    [SerializeField]
+    private float speed = 2.5f, stopDistance, fdist = 10f;
+
+    BrigHP hp;
+    Animator anim;
+
+    Transform player;
+    private bool isFlipped = false;
+    #endregion
+
+    void Awake()
     {
-        
+        #region Components
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        rb = GetComponent<Rigidbody2D>();
+        hp = GetComponent<BrigHP>();
+        anim = GetComponent<Animator>();
+        #endregion
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LookAtPlayer()
     {
-        
+        Vector3 flipped = transform.localScale;
+        flipped.z *= -1f;
+
+        if (transform.position.x > player.position.x && isFlipped)
+        {
+            transform.localScale = flipped;
+            transform.Rotate(0f, 180f, 0f);
+            isFlipped = false;
+        }
+        else if (transform.position.x < player.position.x && !isFlipped)
+        {
+            transform.localScale = flipped;
+            transform.Rotate(0f, 180f, 0f);
+            isFlipped = true;
+        }
+    }
+
+    public void ChasePlayer()
+    {
+        Vector2 target = new Vector2(player.position.x, rb.position.y);
+        Vector2 newPos = Vector2.MoveTowards(rb.position, target, speed * Time.deltaTime);
+        if (Vector2.Distance(transform.position, player.position) < fdist)
+        {
+            rb.MovePosition(newPos);
+            anim.SetBool("Idle", false);
+        }
+        else
+        {
+            anim.SetBool("Idle", true);
+        }
     }
 }
