fbecdcf570d92b4bec9854d4459711e8c241aac9
Carenazz <73272083+Carenazz@users.noreply.github.com>
2021-02-03 13:34:23 +0100

    Update BrigHP.cs

--- Assets/Code/Enemies/Ruins/Brigand/BrigHP.cs
+++ Assets/Code/Enemies/Ruins/Brigand/BrigHP.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class BrigHP : MonoBehaviour
 {
-    // Start is called before the first frame update
+    #region Variables
+    public Animator anim;
+
+    [SerializeField]
+    private int maxHealth = 100, currentHealth;
+
+    BrigMove move;
+    #endregion
+
     void Start()
     {
-        
+        currentHealth = maxHealth;
+        move = GetComponent<BrigMove>();
     }
 
-    // Update is called once per frame
-    void Update()
+    #region Health System
+    public void TakeDamage(int damage)
     {
-        
+        currentHealth -= damage;
+
+        anim.SetTrigger("Hurt");
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
     }
+
+    public void Die()
+    {
+        anim.SetBool("Dead", true);
+
+        this.enabled = false;
+        move.enabled = false;
+    }
+    #endregion
 }
