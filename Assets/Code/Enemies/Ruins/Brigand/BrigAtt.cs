1fe7bb071ae0a204d7f7033a00d7c597399c3f16
Carenazz <73272083+Carenazz@users.noreply.github.com>
2021-02-03 14:52:57 +0100

    Update BrigAtt.cs

--- Assets/Code/Enemies/Ruins/Brigand/BrigAtt.cs
+++ Assets/Code/Enemies/Ruins/Brigand/BrigAtt.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class BrigAtt : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    #region Variables
+    public int attackDamage = 20;
 
-    // Update is called once per frame
-    void Update()
+    public Vector3 attackOffset;
+    public float attackRange = 2f;
+    public LayerMask attackMask;
+    #endregion
+
+    public void Attack()
     {
-        
+        Vector3 pos = transform.position;
+        pos += transform.right * attackOffset.x;
+        pos += transform.up * attackOffset.y;
+
+        Collider2D colInfo = Physics2D.OverlapCircle(pos, attackRange, attackMask);
+        if (colInfo != null)
+        {
+            colInfo.GetComponent<PlayerControls>().TakeDamage(attackDamage);
+        }
     }
 }
